5bc35dd8111e452a167ff99dba4ad55ae3112173
rakibulhasan-joy <rakibulhasanjoy009@gmail.com>
2023-03-23 19:20:16 +0600

    pre view added

--- style.js
+++ style.js
@@ -1,14 +1,18 @@
+// Get the elements from the HTML
 const result = document.getElementById("result");
 const buttons = document.querySelectorAll("button");
 
+// Create variables for the calculator
 let operand1 = "";
 let operand2 = "";
 let operator = "";
 
+// Add click event listeners to all buttons
 buttons.forEach((button) => {
   button.addEventListener("click", (e) => {
     const buttonValue = e.target.innerText;
     
+    // Check if the button clicked is a number
     if (!isNaN(buttonValue) || buttonValue === ".") {
       if (operator === "") {
         operand1 += buttonValue;
@@ -19,15 +23,18 @@ buttons.forEach((button) => {
       }
     }
 
+    // Check if the button clicked is an operator
     if (buttonValue === "+" || buttonValue === "-" || buttonValue === "*" || buttonValue === "/") {
       operator = buttonValue;
     }
 
+    // Check if the button clicked is the equals sign
     if (buttonValue === "=") {
       const num1 = parseFloat(operand1);
       const num2 = parseFloat(operand2);
       let answer = "";
 
+      // Perform the operation based on the operator
       switch (operator) {
         case "+":
           answer = num1 + num2;
@@ -45,13 +52,16 @@ buttons.forEach((button) => {
           break;
       }
 
+      // Display the result
       result.innerText = answer;
 
+      // Reset the variables
       operand1 = answer;
       operand2 = "";
       operator = "";
     }
 
+    // Check if the button clicked is the clear button
     if (buttonValue === "C") {
       result.innerText = "";
       operand1 = "";
